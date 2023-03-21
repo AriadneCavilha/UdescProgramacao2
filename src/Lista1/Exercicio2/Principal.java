68739cafe14f669ed53c83c686e84119d2751005
Ariadne Cavilha <108296310+AriadneCavilha@users.noreply.github.com>
2023-03-21 19:38:42 -0300

    Update Principal.java

--- src/Lista1/Exercicio2/Principal.java
+++ src/Lista1/Exercicio2/Principal.java
@@ -16,6 +16,7 @@ O modificador de escopo ele determina a quem pertece um determinado método ou a
 um atributo ou método for estático, ele vai poder ser acessado de qualquer lugar por qualquer classe, sem a necessidade de instânciar um objeto, ou seja,
 ele não pertence ao objeto em si mas sim a classe, ele facilita quando outras classes vão necessitar daquele método e também para que não seja necessário
 ficar instanciando o tempo todo o método. Isso serve também para os atributos, caso queira utilizar um atributo estático. 
+Garantir que o valor seja o mesmo em todas as intâncias
 */
 public class Principal {
     public static void main(String[] args) {
